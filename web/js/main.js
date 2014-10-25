92791327b1fd6473d7660d863e28e74864980849
jbolda <jbolda@gmail.com>
2014-10-25 14:08:07 -0500

    Fix table for new array style response

--- web/js/main.js
+++ web/js/main.js
@@ -2,21 +2,21 @@ function repoLoaded() {
     var repoTable = document.getElementById('repo-percent-table'),
         repoData = JSON.parse(this.response);
 
-    for (var email in repoData.contributors) {
+    for (var i in repoData.contributors) {
         var tr = document.createElement('tr'),
             gravatarTd = document.createElement('td'),
             gravatarImg = document.createElement('img'),
             emailTd = document.createElement('td'),
             numLinesTd = document.createElement('td'),
             percentTd = document.createElement('td'),
-            emailHash = md5(email.trim().toLowerCase());
+            emailHash = md5(repoData.contributors[i].email.trim().toLowerCase());
 
         gravatarImg.src = 'http://www.gravatar.com/avatar/' + emailHash;
         gravatarImg.height = gravatarImg.width = 80;
 
-        emailTd.innerHTML = email;
-        numLinesTd.innerHTML = repoData.contributors[email];
-        percentTd.innerHTML = repoData.contributors[email] / repoData.totalLines * 100;
+        emailTd.innerHTML = repoData.contributors[i].email;
+        numLinesTd.innerHTML = repoData.contributors[i].lineCount;
+        percentTd.innerHTML = repoData.contributors[i].lineCount / repoData.totalLines * 100;
 
         tr.appendChild(gravatarTd);
         gravatarTd.appendChild(gravatarImg);
